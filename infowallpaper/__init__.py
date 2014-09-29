fa3ef9cf221bad4053d03289cc85b7b90f7b8a86
Martin Ueding <dev@martin-ueding.de>
2014-09-29 16:55:31 +0200

    Move more to config file

--- infowallpaper/__init__.py
+++ infowallpaper/__init__.py
@@ -45,16 +45,17 @@ def main():
 
         print('Generating wallpaper for:', device)
 
-        filename = os.path.expanduser('~/Bilder/Hintergrundbilder/InterfaceLift/01848_flow_2560x1600.jpg')
+        filename = os.path.expanduser(devices[device]['wallpaper'])
 
         im = Image.open(filename)
         draw = ImageDraw.Draw(im)
 
-        x_pos = 850
-        y_pos = 800
+        x_pos = int(devices[device]['x_pos'])
+        y_pos = int(devices[device]['y_pos'])
+        x_offset = int(devices[device]['x_offset'])
 
         draw.text((x_pos, y_pos), device, font=ubuntu_bold)
-        y_pos += 10
+        y_pos += int(devices[device]['y_margin'])
 
         for component in set(devices[device]) - set([x.lower() for x in components_to_list]):
             print('Unknown component:', component)
@@ -62,9 +63,9 @@ def main():
         for component in components_to_list:
             if not component in devices[device]:
                 continue
-            y_pos += 35
+            y_pos += int(devices[device]['y_step'])
             draw.text((x_pos, y_pos), component, font=ubuntu_regular)
-            draw.text((x_pos + 180, y_pos), devices[device][component], font=ubuntu_regular)
+            draw.text((x_pos + x_offset, y_pos), devices[device][component], font=ubuntu_regular)
 
         im.save(device + '.png')
 
